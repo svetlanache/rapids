6ef48766ad17cda60bbf1b83d93c5f392cc7ce9b
Svetlana Cherlin <svetlana.cherlin@gmail.com>
2020-07-07 18:48:02 +0100

    Add CVRS2 functionality

--- R/analyseRealData.R
+++ R/analyseRealData.R
@@ -125,3 +125,157 @@ analyse.realdata = function(datalist, sig = 0.05, group.prop.sig = 0.2,
 
    return(output)
 }
+
+
+#' @title 
+#' Compute p-value for the overall treatment effect and for the 
+#' interaction effect between the treatment and the predicted sensitivity status, for real data.
+#'
+#' @description
+#' significance level of the test for the sensitive group is 
+#' determined by the input proportion of the significance  
+#' level for the sensitive group (group.prop.sig) out of the overall significance (sig).
+#' The p-value for the overall treatment effect is computed using bivariate logistic regression with treatment allocation as a predictor.
+#' The p-value for the interaction effect between the treatment and the predicted sensitivity status is computed using bivariate logistic regression with the treatment effect, the effect for the predicted sensitivity status and the interaction effect. 
+#'
+#' @param datalist A list made up of 3 data frames (patients and covar) and a vector of binary responses, for real data (see \code{realdata} object): patients (a data frame with patients information), covar (a data frame with the covariates), response (a vector of responses).
+#' @param  nclust Number of clusters
+#' @param  sig An overall significance level for the adaptive design.
+#' @param  group.prop.sig Proportion of significance level for the sensitive group test.
+#' @param  seed A  seed for the random number generator.
+#' @param  plotrs An indicator whether to plot the risk scores (default = FALSE).
+#'
+#' @return An object of class \code{"rapids"}.
+#' @return patients: A data frame with patients inormation.
+#' @return pval.resp: P-value for the overall test for the treatment effect w.r.t. response 1
+#' @return pval.resp2: P-value for the overall test for the treatment effect w.r.t. response 2
+#' @return stat.resp.group: Statistic for the interaction effect between the treatment  and the predicted cluster (w.r.t. response ), a vector of length nclust
+#' @return pval.resp.group: P-value for the interaction effect between the treatment and the and the predicted cluster (w.r.t. response 2), a vector of length nclust
+#' @return stat.resp2.group: Statistic for the interaction effect between the treatment and the predicted cluster (w.r.t. response), a vector of length nclust
+#' @return pval.resp2.group: P-value for the interaction effect between the treatment and the predicted cluster (w.r.t. response 2), a vector of length nclust
+
+#' @return estimate.rr: Estimated rate of responsein the sensitive group on the treatment arm.
+#' @return estimate.rr2: Estimated rate of response 2 in the sensitive group on the treatment arm.
+#' @return cvrs: A matrix of the risk scores (rows = patients, columns = simulations).
+#' @return cvrs2: A matrix of the risk scores 2(rows = patients, columns = simulations).
+#' @return cluster.pred: Predicted clusters
+
+#' @author Svetlana Cherlin, James Wason
+
+#' @examples
+#' sig = 0.05
+#' nclust = 4
+#' group.prop.sig = 0.2
+#' seed = 123
+#' plotrs = T
+#' realres.cvrs2 = analyse.realdata2(realdata2, nclust, sig, group.prop.sig, seed, plotrs)
+#'
+#' @seealso
+#' \code{\link{analyse.simdata2}}, \code{\link{simulate.data2}} and \code{\link{cvrs.plot}} functions; \code{\link{print}} and \code{\link{plot}} methods.
+#' @export 
+#' @importFrom "ggplot2" "ggsave"
+
+
+analyse.realdata2 = function(datalist, nclust, sig = 0.05, group.prop.sig = 0.2, 
+                            seed = NULL, plotrs = F)                           
+{  
+   patients = datalist$patients
+   covar = datalist$covar
+   response = datalist$response
+   response2 = datalist$response2
+   
+   if (is.null(dim(patients))) {
+      stop ("Patients data is missing.")
+   } else if (ncol(patients) < 3 ) {
+      stop ("Patients data must have 3 columns (FID, IID, treatment allocation).")
+   }
+   if (is.null(length(response))) {
+      stop ("Response data is missing.")
+   } 
+   if (is.null(length(response2))) {
+      stop ("Response2 data is missing.")
+   }
+   if (is.null(dim(covar))) {
+      stop ("Covariate data is missing.")
+   } 
+   if (!((length(response) == nrow(patients)) & (nrow(patients) == nrow(covar)))) {
+      stop ("Patients data, covariate data and response data dimensions do not match.")
+   }
+   if (!(is.numeric(sig) & (length(sig) == 1) & (sig > 0) & (sig < 1))) {
+      stop ("Significance level sould be between 0 and 1.")
+   }
+   if (!(is.numeric(group.prop.sig) & (length(group.prop.sig) == 1) & (group.prop.sig > 0) & (group.prop.sig < 1))) {
+      stop ("Proportion of significance level for the sensitive group shoud be between 0 and 1.")
+   }   
+
+   sig.group = sig*group.prop.sig
+   sig.overall = sig - sig.group   
+
+   ## P-value for the overall arm comparison
+   mod = tryCatch({vglm(cbind(response, response2) ~ patients$treat, binom2.or)}, error = function(e) e, warning = function(w) w)
+   if (is(mod, "error") | is(mod, "warning")) { 
+      pval.resp = 1
+      pval.resp2 = 1
+   } else if (is.na(logLik(mod))) {
+      pval.resp = 1
+      pval.resp2 = 1
+   } else {
+      sum.mtx = coef(summary(mod))
+      pval.resp = sum.mtx[rownames(sum.mtx) == "patients$treat:1", colnames(sum.mtx) == "Pr(>|z|)"]
+      pval.resp2 = sum.mtx[rownames(sum.mtx) == "patients$treat:2", colnames(sum.mtx) == "Pr(>|z|)"]
+   }
+
+   estimate.rr = numeric (length = nclust)
+   estimate.rr2 = numeric (length = nclust)
+   stat.resp.group = numeric (length = nclust)
+   stat.resp2.group = numeric (length = nclust)
+   pval.resp.group = numeric (length = nclust)
+   pval.resp2.group = numeric (length = nclust)
+
+   ## P-value for the sensitive group test
+   sens = sens.cvrs2(patients, covar, response, response2, seed, nclust)
+
+  for (i in 1:nclust) { ## loop on clusters
+     sens.pred = numeric(length = nrow(patients))
+     sens.pred[sens$cluster.pred == i] = 1
+
+     mod = tryCatch({vglm(cbind(response, response2) ~ sens.pred + patients$treat + sens.pred:patients$treat, binom2.or)}, error = function(e) e, warning = function(w) w)
+     if (is(mod, "error") | is(mod, "warning")) {
+        stat.resp.group[i] = -999
+        pval.resp.group[i] = 1
+        stat.resp2.group[i] = -999
+        pval.resp2.group[i] = 1
+     } else if (is.na(logLik(mod))) {
+        stat.resp.group[i] = -999
+        pval.resp.group[i] = 1
+        stat.resp2.group[i] = -999
+        pval.resp2.group[i] = 1
+     } else {
+        sum.mtx = coef(summary(mod))
+        stat.resp.group[i] = sum.mtx[rownames(sum.mtx) == "sens.pred:patients$treat:1", colnames(sum.mtx) == "z value"]
+        pval.resp.group[i] = sum.mtx[rownames(sum.mtx) == "sens.pred:patients$treat:1", colnames(sum.mtx) == "Pr(>|z|)"]
+        stat.resp2.group[i] = sum.mtx[rownames(sum.mtx) == "sens.pred:patients$treat:2", colnames(sum.mtx) == "z value"]
+        pval.resp2.group[i] = sum.mtx[rownames(sum.mtx) == "sens.pred:patients$treat:2", colnames(sum.mtx) == "Pr(>|z|)"]
+     }
+     resp.group = response[sens.pred == 1] #response for sensitive group
+     resp2.group = response2[sens.pred == 1] #response2 for sensitive group
+     treat.group = patients$treat[sens.pred == 1] #treatment allocation for sensitive group
+
+     resp.treat =  sum(resp.group & treat.group)   #number of responders in the treatment arm
+     nonresp.treat =  sum(!resp.group & treat.group) #number of non-responders in the treatment arm
+     estimate.rr[i] = resp.treat/(resp.treat + nonresp.treat) #estimated response rate in the sensitive group in the treatment arm
+
+     resp2.treat =  sum(resp2.group & treat.group)   #number of responders2 in the treatment arm
+     nonresp2.treat =  sum(!resp2.group & treat.group) #number of non-responders2 in the treatment arm
+     estimate.rr2[i] = resp2.treat/(resp2.treat + nonresp2.treat) #estimated response rate in the sensitive group in the treatment arm
+  } ## end loop on clusters
+
+output = list(patients = patients, pval.resp = pval.resp, pval.resp2 = pval.resp2, stat.resp.group = stat.resp.group, pval.resp.group = pval.resp.group, stat.resp2.group = stat.resp2.group, pval.resp2.group = pval.resp2.group, estimate.rr = estimate.rr, estimate.rr2 = estimate.rr2, cvrs = sens$cvrs, cvrs2 = sens$cvrs2, cluster.pred = sens$cluster.pred)
+   if (plotrs) {
+	   plotcvrs = cvrs2.plot(sens$cvrs, sens$cvrs2, sens$cluster.pred, NULL, FALSE)
+	   ggsave ("cvrs2.pdf", plotcvrs)
+   }
+   class(output) = "rapids"
+   return(output)
+}
+
--- R/analyseSimData.R
+++ R/analyseSimData.R
@@ -62,6 +62,8 @@
 #' @seealso
 #' \code{\link{analyse.realdata}}, \code{\link{simulate.data}} and \code{\link{cvrs.plot}} functions; \code{\link{print}} and \code{\link{plot}} methods.
 #' @author Svetlana Cherlin, James Wason
+#' @importFrom "VGAM" "coef"
+#' @importFrom "VGAM" "summary"
 #' @export 
 #'
 analyse.simdata <- function(datalist, sig = 0.05, group.prop.sig = 0.2, 
@@ -165,3 +167,191 @@ analyse.simdata <- function(datalist, sig = 0.05, group.prop.sig = 0.2,
 
    return(output)
 }
+
+#' @title 
+#' Compute the power for the cross-validate risk scores design for simulated data with two outcomes (CVRS2)
+#'
+#' @description The power is computed for the two outcome variables. For each outcome variable, the power of the adaptive design is comprise of the power for the overall test and  test and the power for the sensitive group test.
+#' The significance level of the test for the sensitive group is 
+#' determined by the input proportion of the significance  
+#' level for the sensitive group (group.prop.sig) out of the overall significance (sig).
+#'
+#' @param datalist A list of 3 data frames that corresponds to the output of the \code{simulate.data2} function (see \code{simdata2} object): patients (a data frame with patients inormation), covar (a data frame with the covariates), response and response2 (data frames of simulated responses).
+#' @param nclust Number of clusters
+#' @param  sig An overall significance level for the adaptive design.
+#' @param  group.prop.sig Proportion of significance level for the sensitive group test.
+#' @param  runs  Number of simulation runs.
+#' @param  seed A  seed for the random number generator.
+#' @param  plotrs An indicator whether to plot the risk scores(default = FALSE).
+#'
+#' @return An object of class \code{"rapids"}.
+#' @return patients: A data frame with patients inormation.
+#' @return pwr.overall: Power for the overall test.
+#' @return pwr.group: Power for the sensitive group test, a vector of length nclust
+#' @return pwr.adaptive: Power for the adaptive design, a vector of length nclust
+#' @return estimate.rr: Empirical rate of response on the experimental arm for the sensitive group, one value per simulation run.
+#' #return estimate.rr2: Empirical rate of response2 on the experimental arm for the sensitive group, one value per simulation run.
+#' @return psens: Sensitivity of identifying the sensitive group, a vector of the length nclust per simulation run.
+#' @return pspec: Specificity of identifying the sensitive group, a vector of the length nclust per simulation run.
+#' @return cluster.pred: A matrix with predicted clusters (rows = patients, columns = simulations).
+#' @return cvrs: A matrix of the risk scores for the response(rows = patients, columns = simulations).
+#' @return cvrs2: A matrix for the risk scores for the response2 (rows = patients, columns = simulations).
+#'
+#' @examples
+#' data(simdata2)
+#' nclust = 4
+#' sig = 0.05
+#' group.prop.sig = 0.2
+#' seed = 123
+#' plotrs = T
+#' simres.cvrs2 = analyse.simdata2 (simdata2, nclust, sig, group.prop.sig, seed, plotrs)
+#'
+#' @seealso
+#' \code{\link{analyse.realdata2}}, \code{\link{simulate.data2}} and \code{\link{cvrs2.plot}} functions; \code{\link{print}} and \code{\link{plot}} methods.
+#' @author Svetlana Cherlin, James Wason
+#' @importFrom "VGAM" "vglm"
+#' @importFrom "VGAM" "binom2.or"
+#' @importFrom "ggplot2" "ggsave"
+#' @export
+#'
+
+analyse.simdata2 <- function(datalist, nclust, sig = 0.05, group.prop.sig = 0.2,
+                             seed = NULL, plotrs = F)
+{
+   patients = datalist$patients
+   covar = datalist$covar
+   response = datalist$response
+   response2 = datalist$response2
+   runs = ncol(response)
+
+   if (is.null(dim(patients))) {
+      stop ("Patients data is missing.")
+   } else if (ncol(patients) < 4 ) {
+      stop ("Patients data must have 4 columns (FID, IID, sensitivity status, treatment allocation).")
+   }
+   if (is.null(dim(response))) {
+      stop ("Response data is missing.")
+   }
+   if (is.null(dim(response2))) {
+      stop ("Response2 data is missing.")
+   }
+   if (is.null(dim(covar))) {
+      stop ("Covariate data is missing.")
+   }
+   for (i in 1:runs) {
+      if (!((nrow(response) == nrow(patients)) & (nrow(patients) == nrow(covar[i,,])) & (nrow(covar[i,,]) == nrow(response2)))) {
+         stop ("Patients data, covariate data, response and response2 data must have the same number of rows.")
+      }
+   }
+   if (!(is.numeric(sig) & (length(sig) == 1) & (sig > 0) & (sig < 1))) {
+      stop ("Significance level sould be between 0 and 1.")
+   }
+   if (!(is.numeric(group.prop.sig) & (length(group.prop.sig) == 1) & (group.prop.sig > 0) & (group.prop.sig < 1))) {
+      stop ("Proportion of significance level for the sensitive group shoud be between 0 and 1.")
+   }
+
+   N = nrow(patients)
+   sig.group = sig*group.prop.sig
+   sig.overall = sig - sig.group
+
+   ## Power for the overall arm comparison 
+   pval.resp = numeric(length = runs)
+   pval.resp2 = numeric(length = runs)
+   for (i in 1:runs) {
+      mod = tryCatch({vglm(cbind(response[,i], response2[,i]) ~ patients$treat, binom2.or)}, error = function(e) e, warning = function(w) w)
+      if (is(mod, "error") | is(mod, "warning")) {
+         pval.resp[i] = 1
+         pval.resp2[i] = 1
+      } else if (is.na(logLik(mod))) {
+         pval.resp[i] = 1
+         pval.resp2[i] = 1
+      } else {
+         sum.mtx = coef(summary(mod))
+         pval.resp[i] = sum.mtx[rownames(sum.mtx) == "patients$treat:1", colnames(sum.mtx) == "Pr(>|z|)"]
+         pval.resp2[i] = sum.mtx[rownames(sum.mtx) == "patients$treat:2", colnames(sum.mtx) == "Pr(>|z|)"]
+      }
+   }
+
+   pwr.resp.overall = sum(pval.resp < sig.overall)/runs
+   pwr.resp2.overall = sum(pval.resp2 < sig.overall)/runs
+
+   sens = list()
+   pval.resp = matrix (nrow = nclust, ncol = runs)
+   pval.resp2 = matrix (nrow = nclust, ncol = runs)
+   estimate.rr = matrix (nrow = nclust, ncol = runs)
+   estimate.rr2 = matrix (nrow = nclust, ncol = runs)
+   for (i in 1:runs) {
+      ## Find clusters
+      sens[[i]] = sens.cvrs2(patients, covar[i,,], response[,i], response2[,i], seed, nclust)
+      if (sens[[i]]$cluster.pred[1] == 0) {
+         return (NA) 
+      } else {
+         for (j in 1:nclust) {
+            ## Each cluster in turn is a sensitive group
+            resp.group = response[sens[[i]]$cluster.pred == j, i] #response for sensitive group
+            resp2.group = response2[sens[[i]]$cluster.pred == j, i] #response2 for sensitive group
+            treat.group = patients$treat[sens[[i]]$cluster.pred == j] #treatment allocation for sensitive group
+
+            conf.resp = matrix(nrow = 2, ncol = 2,
+                data = c(sum(resp.group & !treat.group),  #number of responders in control
+                      sum(!resp.group & !treat.group), #number of non-responders in control
+                      sum(resp.group & treat.group),   #number of responders in treatment
+                      sum(!resp.group & treat.group)), #number of non-responders in treatment
+                byrow = TRUE)
+
+            pval.resp[j, i] = fisher.test(conf.resp, alternative = "two.sided")$p.value
+            estimate.rr[j, i] = conf.resp[2,1]/(conf.resp[2,1] + conf.resp[2,2])
+
+            conf.resp2 = matrix(nrow = 2, ncol = 2,
+                data = c(sum(resp2.group & !treat.group),  #number of responders in control
+                      sum(!resp2.group & !treat.group), #number of non-responders in control
+                      sum(resp2.group & treat.group),   #number of responders in treatment
+                      sum(!resp2.group & treat.group)), #number of non-responders in treatment
+                byrow = TRUE)
+
+            pval.resp2[j, i] = fisher.test(conf.resp2, alternative = "two.sided")$p.value
+            estimate.rr2[j, i] = conf.resp2[2,1]/(conf.resp2[2,1] + conf.resp2[2,2])
+         } ##end loop on clusters
+      } ## if cluster.pred isn't 0
+   } ## end loop on runs
+
+   cvrs = matrix(nrow = N, ncol = runs)
+   cvrs2 = matrix(nrow = N, ncol = runs)
+   rownames(cvrs) = rownames(patients)
+   colnames(cvrs) = 1:ncol(response)
+   sens.pred = matrix(nrow = N, ncol = runs)
+   rownames(sens.pred) = rownames(patients)
+   colnames(sens.pred) = 1:ncol(response)
+   cluster.pred = matrix(nrow = N, ncol = runs)
+   rownames(cluster.pred) = rownames(patients)
+   colnames(cluster.pred) = 1:ncol(response)
+   psens = matrix(nrow = nclust, ncol = runs)
+   pspec = matrix(nrow = nclust, ncol = runs)
+   for (i in seq(runs)) {
+      psens[,i] = sens[[i]]$psens
+      pspec[,i] =  sens[[i]]$pspec
+      cvrs[,i] = sens[[i]]$cvrs
+      cvrs2[,i] = sens[[i]]$cvrs2
+      cluster.pred[,i] = sens[[i]]$cluster.pred
+   }
+   pwr.resp.group = numeric(length = nclust)
+   pwr.resp2.group = numeric(length = nclust)
+   for (i in 1:nclust) {
+      pwr.resp.group[i] = sum(pval.resp[i,] < sig.group)/runs
+      pwr.resp2.group[i] = sum(pval.resp2[i,] < sig.group)/runs
+   }
+
+   ## Overall power of the adaptive design
+   pwr.resp.adaptive = pwr.resp.overall + (1-pwr.resp.overall)*pwr.resp.group
+   pwr.resp2.adaptive = pwr.resp2.overall + (1-pwr.resp2.overall)*pwr.resp2.group
+
+   output = list(patients = patients, pwr.resp.overall = pwr.resp.overall, pwr.resp2.overall = pwr.resp2.overall, pwr.resp.group = pwr.resp.group, pwr.resp2.group = pwr.resp2.group, pwr.resp.adaptive = pwr.resp.adaptive, pwr.resp2.adaptive = pwr.resp2.adaptive, estimate.rr = estimate.rr, estimate.rr2 = estimate.rr2, psens = psens, pspec = pspec, cvrs = cvrs, cvrs2 = cvrs2, cluster.pred = cluster.pred)
+
+   if (plotrs) {
+      plotcvrs = cvrs2.plot(cvrs, cvrs2, cluster.pred, patients$cluster.true, TRUE)
+      ggsave ("cvrs2.png", plotcvrs)
+   }
+   class(output) = "rapids"
+   return(output)
+}
+
